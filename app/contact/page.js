98d247aa62fe6b4142fb86d0c6d7a3598f6eb28a
Eren <eerenn327@hotmail.com>
2025-07-19 20:27:08 +0300

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,13 +1,27 @@
 import React from "react";
-
 import ContactClientPage from "./ContactClient";
 import ContactForm from "./ContactForm";
+import { Bebas_Neue } from "next/font/google";
+
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 function ContactPage() {
   return (
-    <div>
+    <div className='flex flex-col items-center px-4'>
+      <h2
+        className={`${bebas.className} text-red-500 text-6xl text-center animate-pulse my-8`}
+      >
+        İletİşİm
+      </h2>
+
       <ContactClientPage />
-      <ContactForm />
+
+      <div className='flex justify-center w-full mt-10'>
+        <ContactForm />
+      </div>
     </div>
   );
 }
